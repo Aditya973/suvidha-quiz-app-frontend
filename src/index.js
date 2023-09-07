4584ed8e59e0d482bdf386ffc44ae5782ac58a1f
Aditya Tiwari <tiwariaditya973@gmail.com>
2023-09-07 18:03:03 +0530

    updated basename

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
-import QuizHolder from './context/QuizHolder';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      {/* <QuizHolder> */}
+    <BrowserRouter basename='/suvidha-quiz-app-frontend'>
         <App />
-      {/* </QuizHolder> */}
     </BrowserRouter>
   </React.StrictMode>
 );
